ef760e29a32fc737ca672defba87750a7c565986
arkoscr <tutordejava@gmail.com>
2019-10-17 15:20:25 -0600

    Reportes de Excel Reloaded

--- reportes/reporteexcel.php
+++ reportes/reporteexcel.php
@@ -1,8 +1,16 @@
 <?php
 	require '../login/funcs/conexion.php';
   $conexion = conectarDB();
-	$consulta = "SELECT * FROM planes";
+  $consulta = "SELECT * FROM actividades INNER JOIN planes ON actividades.id_instancia = planes.id_instancia INNER JOIN instancias ON actividades.id_instancia = instancias.id_instancia INNER JOIN estados ON planes.id_estado = estados.id_estado"; 
+	$resultado2 = $conexion->query($consulta);
 	$resultado = $conexion->query($consulta);
+	
+	if($resultado2->num_rows > 0 ){
+
+	
+	}
+	
+	
 	if($resultado->num_rows > 0 ){
 
 		date_default_timezone_set('America/Costa_Rica');
@@ -25,11 +33,11 @@
 							 ->setKeywords("reporte planes capacitaciones")
 							 ->setCategory("Reporte excel");
 
-		$tituloReporte = "Planes enviados";
-		$titulosColumnas = array('Instancia', 'Modalidad', 'Nombre', 'Duración', 'Estrategia', 'Tipo de Actividad','Origen','Area','Estado','Costo');
+		$tituloReporte = "Actividades enviadas";
+		$titulosColumnas = array('Instancia', 'Modalidad', 'Nombre', 'Duración', 'Estrategia', 'Tipo de Actividad','Origen','Area','Estado','Sedes','Participantes','Inicio','Finalización','Costo');
 
 		$objPHPExcel->setActiveSheetIndex(0)
-        		    ->mergeCells('A1:J1');
+        		    ->mergeCells('A1:N1');
 
 		// Se agregan los titulos del reporte
 		$objPHPExcel->setActiveSheetIndex(0)
@@ -37,31 +45,51 @@
         		    ->setCellValue('A3',  $titulosColumnas[0])
 		            ->setCellValue('B3',  $titulosColumnas[1])
         		    ->setCellValue('C3',  $titulosColumnas[2])
-								->setCellValue('D3',  $titulosColumnas[3])
+					->setCellValue('D3',  $titulosColumnas[3])
             		->setCellValue('E3',  $titulosColumnas[4])
-                ->setCellValue('F3',  $titulosColumnas[5])
-                ->setCellValue('G3',  $titulosColumnas[6])
-                ->setCellValue('H3',  $titulosColumnas[7])
-								->setCellValue('I3',  $titulosColumnas[8])
-								->setCellValue('J3',  $titulosColumnas[9]);
+					->setCellValue('F3',  $titulosColumnas[5])
+					->setCellValue('G3',  $titulosColumnas[6])
+					->setCellValue('H3',  $titulosColumnas[7])
+					->setCellValue('I3',  $titulosColumnas[8])
+					->setCellValue('J3',  $titulosColumnas[9])
+					->setCellValue('K3',  $titulosColumnas[10])
+					->setCellValue('L3',  $titulosColumnas[11])
+					->setCellValue('M3',  $titulosColumnas[12])
+					->setCellValue('N3',  $titulosColumnas[13]);
 
 		//Se agregan los datos de los planes
 		$i = 4;
+		$k = 0;
 		while ($fila = $resultado->fetch_array()) {
-			$objPHPExcel->setActiveSheetIndex(0)
-        		    ->setCellValue('A'.$i,  utf8_encode($fila['instancia']))
+			   $sedes2="";
+			   $participantes = 0;
+			   $cant_sedes = sizeof(json_decode($fila['sede']));
+			   $jsonObject = json_decode($fila['sede']);
+				for ($m=0; $m < $cant_sedes; $m++) { 
+					$sedes2 .= $jsonObject[$m]->regional .",";
+					$participantes = $participantes + $jsonObject[$m]->cantParticipantes;
+			   			$objPHPExcel->setActiveSheetIndex(0)
+        		    ->setCellValue('A'.$i,  utf8_encode($fila['nombre']))
 		            ->setCellValue('B'.$i,  utf8_encode($fila['modalidad']))
-        		    ->setCellValue('C'.$i,  utf8_encode($fila['nombre']))
+        		    ->setCellValue('C'.$i,  utf8_encode($fila['nombre_actividad']))
 					->setCellValue('D'.$i,  utf8_encode($fila['duracion']))
             		->setCellValue('E'.$i, utf8_encode($fila['estrategia']))
 					->setCellValue('F'.$i, utf8_encode($fila['tipo_actividad']))
 					->setCellValue('G'.$i, utf8_encode($fila['tipo']))
 					->setCellValue('H'.$i, utf8_encode($fila['area']))
-					->setCellValue('I'.$i, utf8_encode($fila['estado']))
-					->setCellValue('J'.$i, utf8_encode($fila['costo']));
+					->setCellValue('I'.$i, utf8_encode($fila['etiqueta_estado']))
+					->setCellValue('J'.$i, utf8_encode($jsonObject[$m]->regional))
+					->setCellValue('K'.$i, utf8_encode($jsonObject[$m]->cantParticipantes))
+					->setCellValue('L'.$i, utf8_encode($jsonObject[$m]->inicio))
+					->setCellValue('M'.$i, utf8_encode($jsonObject[$m]->fin))
+					->setCellValue('N'.$i, utf8_encode($fila['costo']));
 					$i++;
-		}
+				}
+			   $sedes2 = substr($sedes2,0,-1);
+			
 
+					$k++;
+		}
 		$estiloTituloReporte = array(
         	'font' => array(
 	        	'name'      => 'Verdana',
@@ -151,17 +179,17 @@
            	)
         ));
 
-		$objPHPExcel->getActiveSheet()->getStyle('A1:J1')->applyFromArray($estiloTituloReporte);
-		$objPHPExcel->getActiveSheet()->getStyle('A3:J3')->applyFromArray($estiloTituloColumnas);
-		$objPHPExcel->getActiveSheet()->setSharedStyle($estiloInformacion, "A4:J".($i-1));
+		$objPHPExcel->getActiveSheet()->getStyle('A1:N1')->applyFromArray($estiloTituloReporte);
+		$objPHPExcel->getActiveSheet()->getStyle('A3:N3')->applyFromArray($estiloTituloColumnas);
+		$objPHPExcel->getActiveSheet()->setSharedStyle($estiloInformacion, "A4:N".($i-1));
 
-		for($i = 'A'; $i <= 'J'; $i++){
+		for($i = 'A'; $i <= 'N'; $i++){
 			$objPHPExcel->setActiveSheetIndex(0)
 				->getColumnDimension($i)->setAutoSize(TRUE);
 		}
 
 		// Se asigna el nombre a la hoja
-		$objPHPExcel->getActiveSheet()->setTitle('Planes');
+		$objPHPExcel->getActiveSheet()->setTitle('Actividades');
 
 		// Se activa la hoja para que sea la que se muestre cuando el archivo se abre
 		$objPHPExcel->setActiveSheetIndex(0);
