f76186675fbfdbb18cb8150f229502997f93ce67
lchaconc <luis.chacon.campos@gmail.com>
2019-10-23 10:39:38 -0600

    mejora 7 depuracion php
    se depura actulización de archivo

--- main_app/actualizar_archivo.php
+++ main_app/actualizar_archivo.php
@@ -11,30 +11,7 @@
    $urlArchivo = $directorio.$archivo;
 }
   $mysqli = conectarDB();  
-    mysqli_query($mysqli,"UPDATE archivos_enviados SET url = '$urlArchivo' WHERE id_instancia = '$id_instancia'");
-    
-	//En caso de que el usuario suba otro archivo:
-	$rs = mysqli_query($mysqli,"SELECT id_archivo from archivos_enviados ORDER BY id_archivo DESC LIMIT 1");
-        if ($row = mysqli_fetch_row($rs)) {
-        $idArchivo = trim($row[0]);
-      }
-	 
-      echo "<script>console.log('$idArchivo')</script>";
-      //mysqli_query($mysqli,"UPDATE planes SET id_archivo='$idArchivo' WHERE id_instancia = '$id_instancia'") or die ("Problemas al actualizar elementos a la BD".mysqli_error($mysqli));
+    mysqli_query($mysqli,"UPDATE archivos_enviados SET url = '$urlArchivo' WHERE id_instancia = '$id_instancia'");  
+	mysqli_close($mysqli);
 
-
-	  /*update 2 en la tabla planes*/
-  if ($mysqli)
-  {
-  $actualizacionPlan = "UPDATE planes SET archivo_agregado = true WHERE id_instancia = $id_instancia";
-     mysqli_query($mysqli,  $actualizacionPlan  ) or die ("Problemas al añadir elementos a la BD".mysqli_error($mysqli));
-     
-    }
-    else{
-                //Error
-    }
-
-
-
-		$errors = array();
 ?>
